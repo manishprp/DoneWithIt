2da7a3c301c6c9fc916d077628f7366c56d05e2f
Manish.Rajpurohit <Manish.rajpurohit@1rivet.com>
2024-04-25 11:07:07 +0530

    Initial setup and Welcome screen UI

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Image, ImageBackground, StyleSheet, View, Text } from "react-native";
+import colors from "../config/colors";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.registerButton}></View>
+      <View style={styles.loginButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.primary,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    alignItems: "center",
+    position: "absolute",
+    top: 70,
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.secondary,
+  },
+});
+
+export default WelcomeScreen;
